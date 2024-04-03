724fd5e274603b0c6029fae9f41c1266093b74b1
Dev-DaMata <gcordeiro773@gmail.com>
2024-04-02 21:27:38 -0300

    fix: connect mongodb and test a method post create user

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('POST /users', ()=>{
+  it('Register a new user',() =>{
+
+    const user = {
+      name: "Guilherme Cordeiro",
+      email: "guilherme@gmail.com",
+      password: "A123456"
+    }
+
+    cy.task('deleteUser', user.email)
+
+    cy.request({
+      url: '/users',
+      method: 'POST',
+      body: user,
+      failOnStatusCode: false
+    }).then(res =>{
+      expect(res.status).to.eq(200)
+      cy.log(JSON.stringify(res.body))
+    })
   })
 })
