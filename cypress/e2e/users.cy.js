9bc96d7f92e5b4983b4daa9e21e11c3e0bd0941f
Dev-DaMata <gcordeiro773@gmail.com>
2024-04-17 11:19:10 -0300

    feat: added as the test for the post user route

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,5 +1,6 @@
-describe('POST /users', ()=>{
-  it('Register a new user',() =>{
+describe('POST /users', () => {
+
+  it('Register a new user', () => {
 
     const user = {
       name: "Guilherme Cordeiro",
@@ -9,14 +10,72 @@ describe('POST /users', ()=>{
 
     cy.task('deleteUser', user.email)
 
-    cy.request({
-      url: '/users',
-      method: 'POST',
-      body: user,
-      failOnStatusCode: false
-    }).then(res =>{
+    cy.postUser(user).then(res => {
       expect(res.status).to.eq(200)
-      cy.log(JSON.stringify(res.body))
     })
   })
+
+  it('duplicate mail', () => {
+
+    const user = {
+      name: "James Gun",
+      email: "james.gun@gmail.com",
+      password: "A123456"
+    }
+
+    cy.task('deleteUser', user.email)
+
+    cy.postUser(user)
+
+    cy.postUser(user).then(res => {
+
+      const { message } = res.body
+
+      expect(res.status).to.eq(409)
+      expect(message).to.eq("Duplicated email!")
+    })
+  })
+
+  context('required fields', ()=> {
+    let user;
+
+    beforeEach(() => {
+      user = {
+        name: 'Margot Robbie',
+        mail: 'margor@gmail.com',
+        password: 'A123456'
+      }
+    })
+
+    it('name is required', () =>{
+      delete user.name
+
+      cy.postUser(user).then(res =>{
+        const { message } = res.body
+        expect(res.status).to.eq(400)
+        expect(message).to.eq("ValidationError: \"name\" is required. \"email\" is required. \"mail\" is not allowed")
+      })
+    })
+
+    it('email is required', () =>{
+      delete user.mail
+
+      cy.postUser(user).then(res =>{
+        const { message } = res.body
+        expect(res.status).to.eq(400)
+        expect(message).to.eq("ValidationError: \"email\" is required")
+      })
+    })
+
+    it('password is required', () =>{
+      delete user.password
+
+      cy.postUser(user).then(res =>{
+        const { message } = res.body
+        expect(res.status).to.eq(400)
+        expect(message).to.eq("ValidationError: \"email\" is required. \"password\" is required. \"mail\" is not allowed")
+      })
+    })
+  })
+  
 })
